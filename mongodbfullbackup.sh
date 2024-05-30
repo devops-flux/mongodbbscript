e3c915c174e0620f97d8095f62567c2d5f76a5dd
devops-flux <devsopflux@gmail.com>
2024-05-30 14:37:01 +0530

    functionname modifications

--- mongodbfullbackup.sh
+++ mongodbfullbackup.sh
@@ -1,118 +1,108 @@
 #!/usr/bin/bash
-set -x 
+set -x
 
-function  checkbackupfolder(){
+function checkexistingbackupfolderandcreatenew() {
 
-backupfolder=/opt/bkp-$(date +%y%m%d)
+    backupfolder=/opt/bkp-$(date +%y%m%d)
 
-if [ -d $backupfolder ]
-then 
+    if [ -d $backupfolder ]; then
 
-echo   "you can not take backup, ${backupfolder} folder  is already there, please check"
+        echo "you can not take backup, ${backupfolder} folder  is already there, please check"
 
-exit 1
+        exit 1
 
-else
+    else
 
-mkdir ${backup_dir}
-if [ $? -eq 0 ]; then
-     echo "${backup_dir} was created  successfully"
-     else
-     echo "${backup_dir} was failed. to created"
-     exit 1
-fi
+        mkdir ${backup_dir}
+        if [ $? -eq 0 ]; then
+            echo "${backup_dir} was created  successfully"
+        else
+            echo "${backup_dir} was failed. to created"
+            exit 1
+        fi
 
-fi
+    fi
 
-} 
-
-
-function checkbackupfilename() {
-
-compressedfile=/opt/bkp-$(date +%y%m%d).7z
-
-if [ -f $compressedfile ]
-then 
-
-echo   "you can not compress backupfolder,  $compressedfile  is already there, please check"
-
-exit 1
-
-else 
-
-
-backupfolder=/opt/bkp-$(date +%y%m%d)
-
-if [ -d $backupfolder ]
-then
-
-7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on ${compressedfile}   ${backupfolder}/
-
-if [ $? -eq 0 ]; then
-echo " ${backupfolder} compression is    completed successfully"
-else
-echo " ${backupfolder} compression is  failed  "
-exit 1
-fi
-
-
-fi
- 
-
-fi
 }
 
+function checkexistingbackupfilenameandcreatenew() {
+
+    compressedfile=/opt/bkp-$(date +%y%m%d).7z
+
+    if [ -f $compressedfile ]; then
+
+        echo "you can not compress backupfolder,  $compressedfile  is already there, please check"
+
+        exit 1
+
+    else
+
+        backupfolder=/opt/bkp-$(date +%y%m%d)
+
+        if [ -d $backupfolder ]; then
+
+            7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on ${compressedfile} ${backupfolder}/
+
+            if [ $? -eq 0 ]; then
+                echo " ${backupfolder} compression is    completed successfully"
+            else
+                echo " ${backupfolder} compression is  failed  "
+                exit 1
+            fi
+
+        fi
+
+    fi
+}
 
 function databasedump() {
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ];
- then
-  echo "All input parameters must have a value"
-  exit 1
-fi
+    if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+        echo "All input parameters must have a value"
+        exit 1
+    fi
 
-backup_dir="/opt/bkp-$(date +%y%m%d)"
-db_user_name=$1
-#db_password=$2
-db_host=$2
-db_port=$3
+    backup_dir="/opt/bkp-$(date +%y%m%d)"
+    db_user_name=$1
+    #db_password=$2
+    db_host=$2
+    db_port=$3
 
+    checkexistingbackupfolderandcreatenew
 
+    if [ -d "$backup_dir" ]; then
 
-checkbackupfolder
+        for DATABASE_NAME in $(mongosh --quiet --eval "show dbs" | awk {'print $1'}); do
 
+            mongodump --username "" --password "" --host ${db_host} --db ${DATABASE_NAME} --port ${db_port} --out ${backup_dir}
 
-if [ -d "$backup_dir" ]; then
+            if [ $? -eq 0 ]; then
+                echo "database ${DATABASE_NAME}  dump for  was completed successfully."
+            else
+                echo "database ${DATABASE_NAME}  dump was failed. so removing directory  ${backup_dir}"
+                rm -rf ${backup_dir}
+                if [ $? -eq 0 ]; then
+                    echo "${backup_dir} was  successfully removed"
+                else
+                    echo "${backup_dir} was failed to remove remove it manually"
+                    exit 1
+                fi
+                exit 1
+            fi
 
-for DATABASE_NAME in $(mongosh --quiet --eval "show dbs" |awk {'print $1'}); 
-do
- 
-mongodump   --username "" --password "" --host ${db_host} --db ${DATABASE_NAME}  --port ${db_port} --out ${backup_dir}
- 
-if [ $? -eq 0 ]; then
-     echo "database ${DATABASE_NAME}  dump for  was completed successfully."
-     else
-     echo "database ${DATABASE_NAME}  dump was failed. so removing directory  ${backup_dir}"
-     rm -rf ${backup_dir}
-     if [ $? -eq 0 ]; then
-     echo "${backup_dir} was  successfully removed"
-     else
-     echo "${backup_dir} was failed to remove remove it manually"
-     exit 1
-     fi
-     exit 1
-fi
+        done
 
-done
-    
+        checkexistingbackupfilenameandcreatenew
 
-checkbackupfilename
-
-else 
-   echo "$backup_dir does not exist"
-   exit 1 
-fi 
+    else
+        echo "$backup_dir does not exist"
+        exit 1
+    fi
 
 }
 
-databasedump $1 $2 $3 
+databasedump $1 $2 $3
+
+#####################################################################
+# you can run script with below commands as per your credentials
+# ./mongodbfullbackup.sh root  localhost 27017
